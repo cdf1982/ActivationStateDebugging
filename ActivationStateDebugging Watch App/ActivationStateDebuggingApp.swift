6e2ef239bab71373a83242196b720ce2179f489a
Cesare Forelli <cdf1982@icloud.com>
2023-11-19 05:56:15 +0100

    Initial Commit

---
+++ ActivationStateDebugging Watch App/ActivationStateDebuggingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivationStateDebuggingApp.swift
+//  ActivationStateDebugging Watch App
+//
+//  Created by Cesare Forelli on 19/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ActivationStateDebugging_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
